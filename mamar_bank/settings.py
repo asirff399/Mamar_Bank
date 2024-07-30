7370978f3ab39b0263eaad2131550d848cdb5a6a
asirff399 <aintisar48@gmail.com>
2024-07-30 11:25:17 +0600

    database change

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -95,25 +95,25 @@ WSGI_APPLICATION = 'mamar_bank.wsgi.application'
 
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': env("DB_NAME"),
-#         'USER': env("DB_USER"),
-#         'PASSWORD': env("DB_PASSWORD"),
-#         'HOST': env("DB_HOST"),
-#         'PORT': env("DB_PORT"),
-#     }
-# }
-
-# Replace the SQLite DATABASES configuration with PostgreSQL:
 DATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://mamarbank_18t0_user:VRMCp2xRkpggjrPlZjtgSZRew4k8rBWs@dpg-cq8bcfmehbks738gme10-a.oregon-postgres.render.com/mamarbank_18t0',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env("DB_NAME"),
+        'USER': env("DB_USER"),
+        'PASSWORD': env("DB_PASSWORD"),
+        'HOST': env("DB_HOST"),
+        'PORT': env("DB_PORT"),
+    }
 }
+
+# # Replace the SQLite DATABASES configuration with PostgreSQL:
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         # Replace this value with your local database's connection string.
+#         default='postgresql://mamarbank_18t0_user:VRMCp2xRkpggjrPlZjtgSZRew4k8rBWs@dpg-cq8bcfmehbks738gme10-a.oregon-postgres.render.com/mamarbank_18t0',
+#         conn_max_age=600
+#     )
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
